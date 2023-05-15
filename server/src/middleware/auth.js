46b9cf51e665e3e63182bfac8bf7b4654f5d04d8
Edet Emmanuel Asuquo <59479466+eddy1759@users.noreply.github.com>
2023-05-15 23:27:54 +0000

    auth bug fixed

--- server/src/middleware/auth.js
+++ server/src/middleware/auth.js
@@ -1,21 +1,21 @@
 const CONFIG = require('../config/config').jwt;
 const jwt = require('jsonwebtoken');
-const logger = require('../config/logger')
+const logger = require('../config/logger');
 
-const auth = async (req, res, next) {
-    const token = req.headers['authorization'].split(' ')[1]
-    if (!token) {
-        return res.status(401).send('Unauthorized')
-    }
-    try {
-       const decoded = jwt.verify(token, CONFIG.secret)
-       req.user = decoded.user
-       next()
-    } catch (error) {
-        logger.error(error)
-        console.error(error)
-        res.status(401).send("Unauthorized")
-    }
-}
+const auth = async (req, res, next) => {
+  const token = req.headers['authorization'].split(' ')[1];
+  if (!token) {
+    return res.status(401).send('Unauthorized');
+  }
+  try {
+    const decoded = jwt.verify(token, CONFIG.secret);
+    req.user = decoded.user;
+    next();
+  } catch (error) {
+    logger.error(error);
+    console.error(error);
+    res.status(401).send('Unauthorized');
+  }
+};
 
-module.exports = auth
+module.exports = auth;
